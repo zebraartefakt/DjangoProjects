05d2ad7daa54f4ba637132330791ea9b6d5476ba
Martin Eichner <martin.eichnerpb@gmail.com>
2017-08-25 20:30:18 +0200

    did the python shell stuff

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,5 +1,7 @@
+import datetime
+
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
+from django.utils import timezone
 
 
 class Question(models.Model):
@@ -9,6 +11,9 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -17,3 +22,5 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+
